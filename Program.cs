74c20ce412de3e40a08a592ce6d005dbcbc58eec
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-03 21:21:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SequenciaLimites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números inteiros");
+            Console.Write("Primeiro número: ");
+            int numero1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Segundo número: ");
+            int numero2=Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            if(numero2<numero1)
+            Console.WriteLine("Erro! O segundo número é menor que o primeiro");
+            else
+            while (numero1<numero2)
+            {
+                Console.Write($"{numero1} ");
+                numero1=numero1+1;
+            }
+                Console.Write($"{numero2}");
+
+
+
+
+        }
+    }
+}
